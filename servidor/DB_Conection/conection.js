406e82d417ae40351b60ba5aaeb65fdb77319c49
RAUL HUETE SANCHEZ <raul.huete@ahorramas.com>
2023-02-23 16:25:08 +0100

    api in work

--- servidor/DB_Conection/conection.js
+++ servidor/DB_Conection/conection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 
+
+mongoose.set("strictQuery", true);
 const conectDb = async () => {
     try {
         mongoose.connect(process.env.MONGO_DB, {
--- servidor/index.js
+++ servidor/index.js
@@ -1,27 +1,16 @@
 
 const express = require('express')
 const dotenv = require('dotenv').config()
-const mongoose = require('mongoose');
 const  conectDb  = require('./DB_Conection/conection');
 
-
-mongoose.set("strictQuery", false);
-
 const app = express()
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 app.use(express.json()); 
 
 conectDb()
 
-
 app.get('/', (req,res) =>{
     res.send('hola')
 })
 
-app.listen(process.env.PORT, ()=>{console.log(process.env.PORT)})
+app.listen(process.env.PORT, ()=>{console.log('conectado en el peurto' + process.env.PORT)})
